e4e5d74bc86018d855042457160c44bcf1028979
2376780283 <2376780283@qq.com>
2026-09-19 12:37:20 +0800

    feat:添加 setMouseMode jni回调

--- launcher/android/main.cpp
+++ launcher/android/main.cpp
@@ -10,6 +10,7 @@ Copyright (C) 2022 nillerusr
 #include <unistd.h>
 #include <SDL_hints.h>
 #include <SDL_system.h>
+#include <SDL.h>
 #include "tier0/dbg.h"
 #include "tier0/threadtools.h"
 #include "SourceApp/sourceapp_userstats.h"
@@ -138,6 +139,31 @@ void android_property_print(const char *name)
 }
 
 // --------------------------------------------------------------------------------------------
+// purpose: Mouse visibility callback and mode control
+// --------------------------------------------------------------------------------------------
+extern "C" void SetMouseVisibilityHook(void (*callback)(bool));
+
+static void OnMouseVisibilityChangedNative(bool bVisible) {
+    JNIEnv *env = (JNIEnv*)SDL_AndroidGetJNIEnv();
+    if (!env) return;
+
+    jclass clazz = env->FindClass("zzh/source/launcher/data/jni/GameBridge");
+    if (!clazz) return;
+
+    jmethodID method = env->GetStaticMethodID(clazz, "onMouseVisibilityChanged", "(Z)V");
+    if (method) {
+        env->CallStaticVoidMethod(clazz, method, (jboolean)bVisible);
+    }
+    env->DeleteLocalRef(clazz);
+}
+
+DLL_EXPORT void Java_zzh_source_launcher_data_jni_GameBridge_setMouseMode(JNIEnv* env, jclass clazz, jboolean isRelative)
+{
+    SDL_SetRelativeMouseMode(isRelative ? SDL_TRUE : SDL_FALSE);
+}
+
+
+// --------------------------------------------------------------------------------------------
 // purpose: 程序入口
 // --------------------------------------------------------------------------------------------
 DLL_EXPORT int LauncherMainAndroid(int argc, char **argv)
@@ -156,6 +182,9 @@ DLL_EXPORT int LauncherMainAndroid(int argc, char **argv)
 =======
     Msg("[SourceApp]: GetTotalMemory() = %.2f \n", GetTotalMemory());
 >>>>>>> 6acc3351 (feat(all-SourceApp-Jni): 添加类似SteamContext兼容接口（SourceApp）)
+
+    // Register the mouse visibility hook
+    SetMouseVisibilityHook(OnMouseVisibilityChangedNative);
 
     android_property_print("ro.build.version.sdk");
     android_property_print("ro.product.device");
